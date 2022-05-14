c4188ee47b93ab9a36cbc4c23d025b9331ec761c
AMH <1664130911@qq.com>
2022-05-14 11:06:20 +0800

    hot commit

--- src/get_test.java
+++ src/get_test.java
@@ -2,5 +2,6 @@ public class get_test {
     public static void main(String[] args) {
         System.out.println("git-test");
         System.out.println("get-test1");
+        System.out.println("get-test1");
     }
 }
